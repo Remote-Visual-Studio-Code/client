bc6ddf32128c62a8b09693eb55629b02c944c682
Exortions <exortionsmc@gmail.com>
2022-10-14 14:27:40 -0700

    add socket

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,43 @@
 import * as vscode from 'vscode';
 
+import * as sioc from 'socket.io-client';
+
 import logger from './util/logger';
 
+const PROD_URL = 'https://localhost:8080'; // change later
+
+export function connect(port: number, prod: boolean): sioc.Socket {
+    return sioc.connect(prod ? PROD_URL : `http://localhost:${port}`);
+}
+
 export function activate(context: vscode.ExtensionContext) {
-    logger.info('Successfully started Remote Visual Studio Code extension');
+    const env = process.env.NODE_ENV;
+
+    const isProd = env === 'production';
+
+    logger.info(`Starting Remote Visual Studio Code in ${env} enviornment`);
 
     const disposable = vscode.commands.registerCommand('remote-visual-studio-code.helloWorld', () => {
         vscode.window.showInformationMessage('Hello World from Remote Visual Studio Code!');
     });
 
+    const socket = connect(8080, isProd);
+
+    socket.on('connected', (payload: string) => {
+        const { message } = JSON.parse(payload);
+
+        const REQUIRED_MESSAGE = 'Connected to socket server';
+
+        logger.info(
+            `Connected to socket with:\n  message - ${message}\n  required message - ${REQUIRED_MESSAGE}\n  ok - ${
+                message === REQUIRED_MESSAGE
+            } \n  id - ${socket.id}\n  payload - ${payload}`,
+        );
+    });
+
     context.subscriptions.push(disposable);
+
+    logger.info('Successfully started Remote Visual Studio Code extension');
 }
 
 export function deactivate() {
