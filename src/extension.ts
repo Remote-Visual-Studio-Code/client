5ed3f1464468d5552c5455c4d994394fb6b0a243
Exortions <exortionsmc@gmail.com>
2022-10-14 14:00:46 -0700

    lint

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,15 @@
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log(
-    'Congratulations, your extension "remote-visual-studio-code" is now active!'
-  );
+    console.log('Congratulations, your extension "remote-visual-studio-code" is now active!');
 
-  let disposable = vscode.commands.registerCommand(
-    "remote-visual-studio-code.helloWorld",
-    () => {
-      vscode.window.showInformationMessage(
-        "Hello World from Remote Visual Studio Code!"
-      );
-    }
-  );
+    const disposable = vscode.commands.registerCommand('remote-visual-studio-code.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World from Remote Visual Studio Code!');
+    });
 
-  context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+export function deactivate() {
+    return;
+}
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,12 +1,12 @@
-import * as assert from "assert";
+import * as assert from 'assert';
 
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
-suite("Extension Test Suite", () => {
-  vscode.window.showInformationMessage("Start all tests.");
+suite('Extension Test Suite', () => {
+    vscode.window.showInformationMessage('Start all tests.');
 
-  test("Sample test", () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-  });
+    test('Sample test', () => {
+        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    });
 });
