ee0b9848cd2e6fd5afbd3081e4265713680db81a
Exortions <exortionsmc@gmail.com>
2022-10-14 14:00:57 -0700

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -9,38 +9,38 @@ const path = require('path');
 
 /** @type WebpackConfig */
 const extensionConfig = {
-  target: 'node',
-	mode: 'none', // set to production when packaging
+    target: 'node',
+    mode: 'none', // set to production when packaging
 
-  entry: './src/extension.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2'
-  },
-  externals: {
-    vscode: 'commonjs vscode'
-    // modules added here also need to be added in the .vscodeignore file
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader'
-          }
-        ]
-      }
-    ]
-  },
-  devtool: 'nosources-source-map',
-  infrastructureLogging: {
-    level: 'log'
-  },
+    entry: './src/extension.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'extension.js',
+        libraryTarget: 'commonjs2',
+    },
+    externals: {
+        vscode: 'commonjs vscode',
+        // modules added here also need to be added in the .vscodeignore file
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    devtool: 'nosources-source-map',
+    infrastructureLogging: {
+        level: 'log',
+    },
 };
-module.exports = [ extensionConfig ];
+module.exports = [extensionConfig];
