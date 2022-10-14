3df8cadea2d994a7b8e8ecf79e762b9106632dbc
Exortions <exortionsmc@gmail.com>
2022-10-14 14:33:17 -0700

    remove hello world command

--- src/extension.ts
+++ src/extension.ts
@@ -10,16 +10,13 @@ export function connect(port: number, prod: boolean): sioc.Socket {
     return sioc.connect(prod ? PROD_URL : `http://localhost:${port}`);
 }
 
-export function activate(context: vscode.ExtensionContext) {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function activate(_context: vscode.ExtensionContext) {
     const env = process.env.NODE_ENV;
 
     const isProd = env === 'production';
 
     logger.info(`Starting Remote Visual Studio Code in ${env} enviornment`);
-
-    const disposable = vscode.commands.registerCommand('remote-visual-studio-code.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello World from Remote Visual Studio Code!');
-    });
 
     const socket = connect(8080, isProd);
 
@@ -34,8 +31,6 @@ export function activate(context: vscode.ExtensionContext) {
             } \n  id - ${socket.id}\n  payload - ${payload}`,
         );
     });
-
-    context.subscriptions.push(disposable);
 
     logger.info('Successfully started Remote Visual Studio Code extension');
 }
