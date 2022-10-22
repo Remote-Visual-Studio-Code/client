47d6b86ff7967adf7d821620283b6021c1081aff
Exortions <exortionsmc@gmail.com>
2022-10-22 09:18:28 -0700

    wait for sid before generating token

--- src/util/token.ts
+++ src/util/token.ts
@@ -3,16 +3,24 @@ import * as vscode from 'vscode';
 import retrySocketConnection from './retrySocketConnection';
 import { getSocket, getStorage } from '../extension';
 
-export async function generateToken(): Promise<void> {
+export async function generateToken(waitForSidCreation = false): Promise<void> {
     await retrySocketConnection();
 
     const socket = getSocket();
 
     if (!socket.connected) return;
 
-    const sid = getStorage().get('sid');
+    let sid = getStorage().get('sid');
 
-    if (!sid || sid === null) return;
+    if (!waitForSidCreation) {
+        if (!sid || sid === null) return;
+    } else {
+        while (!getStorage().get('sid') || getStorage().get('sid') === null) {
+            await new Promise((resolve) => setTimeout(resolve, 100));
+        }
+
+        sid = getStorage().get('sid');
+    }
 
     socket.emit('token.generate', JSON.stringify({ sid: sid }));
 
